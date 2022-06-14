98cddfac531317b978af5645fc9c3d485b2b73e6
Mrsazza <shahriarhossain147@gmail.com>
2022-06-14 13:50:40 +0600

    Initial Commit

---
+++ ICD-10/ICD_10App.swift
@@ -0,0 +1,17 @@
+//
+//  ICD_10App.swift
+//  ICD-10
+//
+//  Created by Sazza on 14/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ICD_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
